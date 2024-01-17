a2ab2ae910e160d88b9d2e89e7a29a5f556937f5
anjaemun <ajm0111@naver.com>
2024-01-17 14:38:13 +0900

    20240117 findById 까지

--- src/main/java/com/example/book/controller/BookController.java
+++ src/main/java/com/example/book/controller/BookController.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -15,7 +14,7 @@ import com.example.book.service.BookService;
 
 @Controller
 public class BookController {
-	
+
 	@Autowired
 	private BookService bookService;
 
@@ -23,31 +22,31 @@ public class BookController {
 	public String save() {
 		return "save";
 	}
-	
+
 	@PostMapping("/save")
 	public String save(BookDTO bookDTO) {
 		System.out.println(bookDTO);
 		bookService.save(bookDTO);
-		
+
 		// 단순하게 list.html만 요청
 		// return "list";
 		// redirect : 컨트롤러의 메서드에서 다른 메서드의 주소를 요청하고자 할 
 		return "redirect:/list";
 	}
-	
+
 	@GetMapping("/list")
 	public String findAll(Model model) {
 		List<BookDTO> bookDTOs = bookService.findAll();
 		model.addAttribute("bookList", bookDTOs);
 		return "list";
 	}
-	
-	@GetMapping("/list/{id}")
+
+	@GetMapping("/book/{id}")
 	public String findById(@PathVariable("id") Long id, Model model) {
 		BookDTO bookDTO = bookService.findById(id);
-		model.addAttribute(bookDTO);
+		model.addAttribute("book",bookDTO);
 		return "detail";
-		
+
 	}
-	
+
 }
